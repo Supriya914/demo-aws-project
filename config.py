ba896e6613536b539d018055c26867064907c798
Supriya914 <99598882+Supriya914@users.noreply.github.com>
2022-04-04 17:58:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "mydatabase.ci4td1u42qon.us-east-1.rds.amazonaws.com"
+customhost = "mydb.ckqyixthfjry.us-east-1.rds.amazonaws.com"
 customuser = "admin"
 custompass = "admin123"
 customdb = "mydatabase"
-custombucket = "my-project2-bucket"
+custombucket = "my-project-bucket11"
 customregion = "us-east-1"
